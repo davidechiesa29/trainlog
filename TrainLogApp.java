a62a3c792ffbf8197ccea46aa4fc09b39191c006
Davide <davidechiesa29@gmail.com>
2025-05-17 23:28:50 -0400

    Added menu buttons and app name to home screen

--- TrainLogApp.java
+++ TrainLogApp.java
@@ -4,6 +4,9 @@ import javafx.scene.control.Button;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.application.Platform;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 
 public class TrainLogApp extends Application {
 
@@ -12,11 +15,20 @@ public class TrainLogApp extends Application {
 	}
 
 	public void start(Stage stage) {
-		Button b = new Button("Log New Activity");
+
+		Label appName = new Label("Train Log");
+		Button newActivity = new Button("Log Activity");
+		Button viewActivities = new Button("View Activities");
+		Button statistics = new Button("View Statistics");
+		Button records = new Button("View Personal Records");
 		Button quit = new Button("Quit");
-		Group g = new Group(b, quit);
+		VBox menu = new VBox(newActivity, viewActivities, statistics, records, quit);
+		BorderPane home = new BorderPane();
+		home.setCenter(menu);
+		home.setTop(appName);
+		
 		quit.setOnAction(event -> Platform.exit());
-		stage.setScene(new Scene(g, 1000, 650));
+		stage.setScene(new Scene(home, 1000, 650));
 		stage.show();
 	}
 
