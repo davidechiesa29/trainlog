638cb1251e92afec80053b7bbb32326c4f552777
Davide <davidechiesa29@gmail.com>
2025-06-05 21:46:12 -0400

    Added better documentation to the Activity and ActivityManager files

--- ButtonActions/Activity.java
+++ ButtonActions/Activity.java
@@ -19,6 +19,9 @@ public class Activity implements Comparable<Activity> {
 	public boolean weatherData;
 	public boolean dynamicTitle;
 
+	/**
+	 * Stores all of the necessary information for the activity
+	 */
 	public Activity (String title, double distance, int duration, Integer heartrate, String description, String location, String date, String time, 
 			String runType, boolean weatherData, boolean dynamicTitle) {
 		this.distance = distance;
@@ -33,22 +36,31 @@ public class Activity implements Comparable<Activity> {
 		this.dynamicTitle = dynamicTitle;
 		this.title = title;
 
+		// Replaces the previous title with an AI-generated one if requested by the user
 		if (this.dynamicTitle == true) this.title = generateActivityTitle(this);
 	}
 
+	/**
+	 * Compare-to method for sorting activities according to how recently the activity was completed
+	 */
 	@Override
 	public int compareTo(Activity other) {
 
+		// Parses the date and time of the activities
 		String[] thisDate = this.date.split("-");
 		String[] otherDate = other.date.split("-");
 		String[] thisTime = this.time.split("-");
 		String[] otherTime = other.time.split("-");
 
+		// Converts months into an integer 
 		thisDate[0] = convertMonthToInteger(thisDate[0]);
 		otherDate[0] = convertMonthToInteger(otherDate[0]);
+
+		// Converts 'AM' into 0 and 'PM' to 1
 		thisTime[2] = convertTimeToInteger(thisTime[2]);
 		otherTime[2] = convertTimeToInteger(otherTime[2]);
 
+		// Compares the date
 		if (Integer.parseInt(thisDate[2]) > Integer.parseInt(otherDate[2]))
 			return -1;
 		else if (Integer.parseInt(thisDate[2]) < Integer.parseInt(otherDate[2]))
@@ -62,6 +74,7 @@ public class Activity implements Comparable<Activity> {
 		else if (Integer.parseInt(thisDate[1]) < Integer.parseInt(otherDate[1]))
                         return 1;
 		
+		// Compares the time if the activities have the same date
 		if (Integer.parseInt(thisTime[2]) > Integer.parseInt(otherTime[2]))
 			return -1;
 		else if (Integer.parseInt(thisTime[2]) < Integer.parseInt(otherTime[2]))
@@ -78,6 +91,9 @@ public class Activity implements Comparable<Activity> {
 		return 0;
 	}
 
+	/**
+	 * Returns integer value corresponding to each month
+	 */
 	private String convertMonthToInteger(String s){
 		if (s.equals("January")) return "1";
 		if (s.equals("Febuary")) return "2";
@@ -93,16 +109,24 @@ public class Activity implements Comparable<Activity> {
 		return "12";
 	}
 
+	/**
+	 * Returns integer value corresponding to 'AM' or 'PM'
+	 */
 	private String convertTimeToInteger(String s){
 		if (s.equals("AM")) return "0";
 		return "1";
 	}
 
+	/**
+	 * Creates an AI-Generated title for an activity, getting inspiration from the activity time, heart-rate, distance, duration, and run-type
+	 */
 	public static String generateActivityTitle(Activity a) {
 
-
+		
+		// Stores API key to access openAI API
 		String apiKey = "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";
 
+		// Generates the body of the request that will be sent to chatGPT in JSON format
 		String body = """
 		{
 			"model": "gpt-4o-mini",
@@ -113,12 +137,13 @@ public class Activity implements Comparable<Activity> {
 				}
 			]
 		}""";
-
 		body = body.replace("placeholder",generatePrompt(a)); 
 
+		// Formally creates the reqeust, specifying the API key 
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.openai.com/v1/chat/completions")).header("Content-Type","application/json").header("Authorization","Bearer " + apiKey).POST(HttpRequest.BodyPublishers.ofString(body)).build();
 		
 		try {
+			// Attempts to get a response from open AI, and then parses the content out of the response and returns it
 			HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 			Gson parsed = new Gson();
 		        Output output =  parsed.fromJson(response.body(),new TypeToken<Output>(){});
@@ -126,14 +151,20 @@ public class Activity implements Comparable<Activity> {
 			
 
 		} catch (Exception e){
+
+			// Catches potential errors that may be caused by connected to the API
 			System.out.println("Error fetching dynamically generated title, resorting to default title name");
 		}
 
+		// Returns default activity title if dynamic title generation failed
 		return "Daily Run";
 
 
 	}
 
+	/**
+	 * Creates the specific prompt which will be fed to chatGPT
+	 */
 	private static String generatePrompt(Activity activity){
 		String prompt = "Generate a creative title for a running activity." + 
 			"Use the following information to inspire the title, but do not" + 
@@ -146,6 +177,10 @@ public class Activity implements Comparable<Activity> {
 		return prompt;
 	}
 
+
+	/**
+	 * Inner classes used for parsing the content out of the Json format and directly into an object
+	 */
 	class Output {
 
 		ArrayList<Choice> choices;
--- ButtonActions/ActivityManager.java
+++ ButtonActions/ActivityManager.java
@@ -7,23 +7,36 @@ import java.io.*;
 
 public class ActivityManager {
 
+	// Used for directly storing activities, with activities sorted by how recently they occurred
 	private static PriorityQueue<Activity> activities = new PriorityQueue<>();
 
+	/**
+	 * Adds the specified activity and saves it 
+	 */
 	public static void add(Activity a) {
 		activities.add(a);
 		saveActivities();
 	}
 
+	/**
+	 * Removes the specified activity and saves the new list of activities
+	 */
 	public static boolean remove(Activity a){
 		boolean removed = activities.remove(a);
 		saveActivities();
 		return removed;
 	}
 
+	/**
+	 * Returns the entire list of activities
+	 */
 	public static PriorityQueue<Activity> getAll() {
 		return activities;
 	}
 
+	/**
+	 * Intializes the priority queue by loading data stored in a Json file onto it
+	 */ 
 	public static void retrieveSavedActivities() {
 		try {
 			Gson gson = new Gson();
@@ -36,7 +49,9 @@ public class ActivityManager {
 		}	
 	}
 
-
+	/**
+	 * Saves the user's activities by writing them to a Json file 
+	 */ 
 	public static void saveActivities() {
 		List<Activity> saved = new ArrayList<>(activities);
 		Gson gson = new Gson();
